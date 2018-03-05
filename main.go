fdf5ec046497c31ccb7c37fd5acd95e26a45249f
Masaki Tsuchiya <ikasamt@gmail.com>
2018-03-05 15:49:17 +0900

    change resize args

--- main.go
+++ main.go
@@ -21,31 +21,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	fileName := os.Args[1]
-	baseFileName := filepath.Base(fileName)
+	for _, fileName := range os.Args[1:] {
+		baseFileName := filepath.Base(fileName)
 
-	file, err := os.Open(fileName)
-	defer file.Close()
-	if err != nil {
-		log.Fatal(err)
+		file, err := os.Open(fileName)
+		defer file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		img, formatName, err := image.Decode(file)
+		if err != nil {
+			log.Println(fileName)
+			log.Println(formatName)
+			log.Fatal(err)
+		}
+
+		// resize
+		imgThumb := resize.Thumbnail(500, 500, img, resize.Bicubic)
+
+		// save
+		outputFileName := fmt.Sprintf("r500x500_%s", baseFileName)
+		out, err := os.Create(outputFileName)
+		defer out.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		jpeg.Encode(out, imgThumb, nil)
 	}
-
-	img, formatName, err := image.Decode(file)
-	if err != nil {
-		log.Println(fileName)
-		log.Println(formatName)
-		log.Fatal(err)
-	}
-
-	// resize
-	imgThumb := resize.Thumbnail(400, 400, img, resize.Lanczos3)
-
-	// save
-	outputFileName := fmt.Sprintf("r300x300_%s", baseFileName)
-	out, err := os.Create(outputFileName)
-	defer out.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	jpeg.Encode(out, imgThumb, nil)
 }
